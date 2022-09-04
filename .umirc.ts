fd326add4f96011c7186682ef7532b62334b3002
Huang Daoxu <34485325+sanjayheaven@users.noreply.github.com>
2022-09-04 12:13:58 +0800

    initial commit

---
+++ .umirc.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'Motion UI',
+  mode: 'site',
+  // more config: https://d.umijs.org/config
+  scripts: [
+    `https://cdn.tailwindcss.com`,
+    `
+      tailwind.config = {
+        corePlugins: {
+          preflight: false,
+        },
+      }
+    `,
+  ],
+
+  navs: {
+    'zh-CN': [
+      { title: '指南', path: '/zh-CN/guide' },
+      { title: '组件', path: '/zh-CN/components' },
+      { title: 'GitHub', path: 'https://github.com/sanjayheaven/motion-ui' },
+    ],
+    'en-US': [
+      { title: 'Guide', path: '/guide' },
+      { title: 'Components', path: '/components' },
+      { title: 'GitHub', path: 'https://github.com/sanjayheaven/motion-ui' },
+    ],
+  },
+});
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# motion-ui
-A React UI library without opinionated UI. 
+# dumi app
+
+## Getting Started
+
+Install dependencies,
+
+```bash
+$ npm i
+```
+
+Start the dev server,
+
+```bash
+$ npm start
+```
+
+Build site app,
+
+```bash
+$ npm run build
+```
