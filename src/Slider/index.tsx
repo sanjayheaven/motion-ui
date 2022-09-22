fc339eb9e444985f7ec9558233c8869cdda8055c
Huang Daoxu <34485325+sanjayheaven@users.noreply.github.com>
2022-09-22 14:03:54 +0800

    chore: update Slider - add value transform and click event

--- src/Slider/index.tsx
+++ src/Slider/index.tsx
@@ -1,11 +1,19 @@
 import { useThrottleFn } from "ahooks";
 import {
+  animate,
   AnimatePresence,
   motion as Motion,
   useMotionValue,
 } from "framer-motion";
 
-import { useEffect, useRef, useState } from "react";
+import React, {
+  MouseEventHandler,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { useRect } from "../_common/hooks/useRect";
 import { useWidth } from "../_common/hooks/useWidth";
 
 import { fadeIn } from "../_common/utils/presets";
@@ -16,69 +24,100 @@ export default function Slider({
   trail,
   handle,
   value,
+  range = false,
   min = 0,
   max = 100,
+  step = 1,
+  className = "",
   onChange,
+  onAfterChange,
 }: ISliderProps) {
-  const [innerPercent, setInnerPercent] = useState(0);
+  const [innerValue, setInnerValue] = useState(0);
   const barRef = useRef<HTMLDivElement>(null);
   const trailRef = useRef<HTMLDivElement>(null);
   const handleRef = useRef<HTMLDivElement>(null);
 
+  const handleChange = (value: number) => {
+    let newValue = value;
+    newValue = newValue < min ? min : newValue;
+    newValue = newValue > max ? max : newValue;
+    setInnerValue(newValue);
+    onChange?.(newValue);
+  };
   const barWidth = useWidth(barRef);
-  // const trailWidth = useWidth(trailRef);
-  // const handleWidth = useWidth(handleRef);
+  const trailWidth = useWidth(trailRef);
+  const handleWidth = useWidth(handleRef);
+
+  const barRect = useRect(barRef);
 
   const x = useMotionValue(0);
 
-  /** calc transform-x by value  */
+  /** calc transform-x by value */
   const calcX = (value: number) => {
     return ((value - min) / (max - min)) * barWidth;
   };
 
+  /** calc value by transform-x */
+  const calcValue = (x: number) => {
+    if (!barWidth) return 0;
+    return (x / barWidth) * (max - min) + min;
+  };
+
   useEffect(() => {
     const transformX = calcX(value);
-    x.set(transformX);
-  }, [value, barWidth]);
+    x.set(transformX); // do not use animate here, here only do data Synchronize
+  }, [value, barWidth, min, max]);
 
-  useThrottleFn(
+  const handleXChange = () => {
+    const currentX = x.get();
+    const newValue = Math.round(calcValue(currentX));
+    if (innerValue == newValue) return newValue;
+    console.log(newValue, currentX, innerValue);
+    handleChange(newValue);
+    return newValue;
+  };
+
+  const { run } = useThrottleFn(
     () => {
-      console.log("");
+      handleXChange();
     },
-    { wait: 120 }
+    { wait: 0 }
   );
 
   useEffect(() => {
-    const unsubscribeX = x.onChange(() => {
-      const roundedValue = Math.round(x.get());
-
-      if (innerPercent == roundedValue) return;
-      // console.log(innerPercent, roundedValue, value);
-
-      onChange?.(roundedValue);
-    });
+    // here, may re-create a new onXChange. Thus will not get latest value
+    const unsubscribeX = x.onChange(run);
     return () => unsubscribeX();
   }, []);
 
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const clientX = e.clientX;
+    const barClientX = barRect.left;
+    const newTransformX = clientX - barClientX;
+    // x.set(newTransformX);
+    animate(x, newTransformX); // whether  should open this animation ????
+    console.log(clientX, barClientX, newTransformX, "handleClick");
+  };
   useEffect(() => {
-    if (value <= 0) return setInnerPercent(0);
-    if (value >= 100) return setInnerPercent(100);
-    setInnerPercent(value);
+    if (value <= min) return handleChange(min);
+    if (value >= max) return handleChange(max);
+    setInnerValue(value);
   }, [value]);
 
   return (
     <AnimatePresence>
-      <Motion.div className=" relative" {...fadeIn}>
-        <div onClick={console.log} ref={barRef}>
+      <Motion.div className=" relative w-full" {...fadeIn}>
+        <div onClick={(e) => handleClick(e)} ref={barRef}>
           {bar}
         </div>
 
-        {(innerPercent >= 0 && (
+        {(innerValue >= 0 && (
           <>
             {/* trail node */}
             <Motion.div
+              initial={{ width: 0 }}
               ref={trailRef}
-              onClick={console.log}
+              onClick={(e) => handleClick(e)}
               className={` absolute top-0 bottom-0 h-full `}
               style={{ width: x }}
             >
@@ -89,17 +128,22 @@ export default function Slider({
             <Motion.div
               ref={handleRef}
               drag="x"
+              dragElastic={false}
+              dragMomentum={false}
+              // dragConstraints={barRef}
+              // can use useTransform here.  to calc barwidth
               dragConstraints={{ left: 0, right: barWidth }}
-              onDragEnd={() => {
-                onChange?.(x.get());
+              onDragEnd={(e, { offset }) => {
+                console.log("dragend");
+                console.log(offset.x);
+
+                // let newValue = handleXChange();
+                // newValue = newValue < min ? min : newValue;
+                // newValue = newValue > min ? max : newValue;
+                // onAfterChange?.(newValue);
               }}
               className=" absolute top-1/2"
-              style={{
-                position: "absolute",
-                translateY: "-50%",
-                translateX: "-50%",
-                x,
-              }}
+              style={{ translateY: "-50%", translateX: "-50%", x }}
             >
               {typeof handle === "function" ? handle({ value }) : handle}
             </Motion.div>
--- src/Slider/type.ts
+++ src/Slider/type.ts
@@ -7,12 +7,15 @@ interface ITailFunction {
 type IMark = Record<number, ReactNode>;
 
 export interface ISliderProps {
+  /** bar(wrapper) className */
+  className?: string;
   /** slider value */
   value?: number;
 
   /** bar child node */
   bar?: ReactNode;
-  /** trail child node */
+
+  /** trail child node */ // change Name to track ???
   trail?: ReactNode | (({ value }: ITailFunction) => ReactNode);
 
   /** handle childr node */
@@ -22,14 +25,17 @@ export interface ISliderProps {
   marks?: IMark[];
 
   /** callback function when value change */
-  onChange?: (value?: number) => VoidFunction;
+  onChange?: (value?: number) => void;
+
+  /** callback function to get the latest value after change  */
+  onAfterChange?: (value?: number) => void;
 
   /** min value  */
   min?: number;
-
   /** max value */
   max?: number;
-
   /** step */
   step?: number;
+  /** range mode, two handles exist */
+  range?: boolean;
 }
