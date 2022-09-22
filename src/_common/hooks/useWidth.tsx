2655646dd58f67dadd520286f910334adf6203b6
Huang Daoxu <34485325+sanjayheaven@users.noreply.github.com>
2022-09-22 14:03:02 +0800

    chore: update useWidth hook

--- src/_common/hooks/useWidth.tsx
+++ src/_common/hooks/useWidth.tsx
@@ -1,12 +1,8 @@
 import { useSize } from "ahooks";
-import { RefObject, useMemo } from "react";
+import { RefObject } from "react";
 
 /** get a dom width from a ref object */
 export const useWidth = (wrapperRef: RefObject<HTMLDivElement>) => {
   const wrapperSize = useSize(wrapperRef);
-  const wrapperWidth = useMemo(
-    () => wrapperSize?.width || 0,
-    [wrapperSize?.width]
-  );
-  return wrapperWidth;
+  return wrapperSize?.width || 0;
 };
